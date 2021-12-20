cd3b0b6f58c6eb113562a8180742abd60cc5a703
xuxuejin <xuxuejin@dae.org>
2021-12-20 11:23:05 +0800

    update:更新日志收集相关

--- src/api/index.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-export const getHomeData = () => {
-  return axios.get("https://api.apiopen.top/getJoke?page=1&count=5&type=video");
-};
-
-export const getDetailData = () => {};
-export const getNewsData = () => {};
--- src/server/index.js
+++ src/server/index.js
@@ -1,12 +1,37 @@
 import express from "express";
-import proxy from "express-http-proxy";
+// import proxy from "express-http-proxy";
 import { PORT } from "@/utils/config";
 import render from "./render";
+import morgan from 'morgan';
+// import { createStream } from 'rotating-file-stream';
+import fs from 'fs'
+import path from 'path'
 import axios from "axios";
 const app = express();
 const OPENAPI = "https://api.apiopen.top";
 // 静态资源ok
 app.use(express.static("public"));
+
+// 日志收集
+const rfs = require("rotating-file-stream");
+const pad = num => (num > 9 ? "" : "0") + num;
+const generator = (time, index) => {
+  if (!time) return "file.log";
+
+  var month = time.getFullYear() + "" + pad(time.getMonth() + 1);
+  var day = pad(time.getDate());
+  var hour = pad(time.getHours());
+  var minute = pad(time.getMinutes());
+
+  return `${month}/${month}${day}-${hour}${minute}-${index}-file.log`;
+};
+
+const stream = rfs.createStream(generator, {
+  size: "10M",
+  interval: "3s"
+});
+
+app.use(morgan('combined', { stream }));
 
 // node 中间层处理数据
 app.use("/api/getAsyncData", (req, res) => {
